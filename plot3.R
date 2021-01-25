3c256751ec4fae0d3b7ddbc3bece7a347508bb5a
vmijat <vmijatovic@gmail.com>
2021-01-25 16:52:47 +0100

    plot 3

---
+++ plot3.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(ggplot2)
+
+
+#let's import data into dataframes
+
+scc <- readRDS("./_data/Source_Classification_Code.rds")
+summarySCC <- readRDS("./_data/summarySCC_PM25.rds")
+
+str(summarySCC)
+
+
+
+#different sources (point, nonpoint, onroad, nonroad) - 
+# which one has decreased?
+
+total_emissions_Baltimore_per_type <- summarySCC %>% 
+        filter(fips == "24510") %>% 
+        filter(Pollutant == "PM25-PRI") %>% 
+        group_by(year, type) %>% 
+        summarise(total = sum(Emissions, na.rm = TRUE))
+
+ggplot(data = total_emissions_Baltimore_per_type, aes(x = year, 
+                                                      y = total,
+                                                      color = type)) +
+        geom_point() +
+        geom_smooth() +
+        facet_wrap(.~type)
+
+
+#cliearly, NON-ROAD, NON-POINT and ON-ROAD have decreased
